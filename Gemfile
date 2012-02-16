59c610c901f5b4804088031938e5442450bf0895
Ben Tillman <ben.tillman@gmail.com>
2012-02-16 17:24:24 +1300

    Replaced is_paranoid functionality with paper_trail

--- Gemfile
+++ Gemfile
@@ -1,12 +1,14 @@
 source 'http://rubygems.org'
 
 gem 'rails'
+
+gemspec
+
 gem 'prototype-rails'
 gem 'paperclip'
 gem 'simple_form'
 gem 'will_paginate'
-
-gemspec
+gem 'paper_trail'
 
 gem 'ruby-debug',   :platform => :mri_18
 gem 'ruby-debug19', :platform => :mri_19
--- lib/fat_free_crm/engine.rb
+++ lib/fat_free_crm/engine.rb
@@ -1,9 +1,9 @@
 module FatFreeCRM
-  class Engine < Rails::Engine
+  class Engine < ::Rails::Engine
     config.autoload_paths += Dir[root.join("app/models/**")]
     
     config.to_prepare do
-      %w(is_paranoid country_select dynamic_form gravatar_image_tag responds_to_parent).each do |plugin|
+      %w(country_select dynamic_form gravatar_image_tag responds_to_parent).each do |plugin|
         $:.unshift File.join(File.dirname(__FILE__), '..', plugin, 'lib')
         require plugin
         require File.join(File.dirname(__FILE__), '..', plugin, 'init')
